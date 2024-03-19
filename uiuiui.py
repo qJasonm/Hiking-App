62cebc8244265e9755c4065ae4d781d655bc764b
qJasonm <117887620+qJasonm@users.noreply.github.com>
2024-03-18 19:07:22 -0600

    Update uiuiui.py

--- uiuiui.py
+++ uiuiui.py
@@ -2,19 +2,26 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('whatever.kv')
+Builder.load_file('my.kv')
 
 # Grid layout
-class MyGridLayout(Widget):
-    name = ObjectProperty(None)
+class SigninPage(Screen):
+    pass
 
-    def press(self):
-        print('continue as guess pressed')
+class GuestScreen(Screen):
+    def print_passcode(self):
+        print(f'Entered passcode is: {self.ids.passcode.text}')
 
-class AwesomeApp(App):
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class TrailApp(App):
     def build(self):
-        return MyGridLayout()
+        return ScreenManagement()
 
 if __name__ == '__main__':
-    AwesomeApp().run()
+    TrailApp().run()
