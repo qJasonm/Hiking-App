3fdd559eb3b1c0dfaec9aa24c342a8226a3314f0
Julia1588 <congchuadochoi@gmail.com>
2024-03-27 23:19:14 -0600

    knowledge screen

--- uiuiui.py
+++ uiuiui.py
@@ -5,14 +5,14 @@ from kivy.uix.label import Label
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from weather import Weather
 
 Builder.load_file('my.kv')
 
 class SigninPage(Screen):
     def go_to_knowledge(self, trail_code):
         self.manager.current = 'knowledge'
-        self.manager.get_screen('knowledge').update_trail_code(trail_code)
+        self.manager.get_screen('knowledge').update_data(trail_code)
 
 class GuestScreen(Screen):
     def print_passcode(self, passcode):
@@ -23,25 +23,33 @@ class KnowledgeScreen(Screen):
         super(KnowledgeScreen, self).__init__(**kwargs)
         self.trail_code = '' 
 
-    def update_trail_code(self, trail_code_input):
+    def update_data(self, trail_code_input):
         self.ids.trail_code_display.text = 'Trail Code: ' + trail_code_input
+        weather = Weather()
+        if trail_code_input == '123':
+            weather_condition, weather_temp = weather.get_weather("denver")
+            self.ids.weather.text = str(weather_temp) + "  oF" + "\n" + weather_condition
+            self.ids.alert.text = "Alert: " + "Strong winds"
+            self.ids.more_info.text = "More Info: " + "Nice trail ahead !"
+            self.ids.warning.text = "Warning: " + "Beware of Bears"
+            self.ids.map_image.source = "cherry-creek.png"
 
-class MapWidget(Screen):
-    def __init__(self, **kwargs):
-        super(MapWidget, self).__init__(**kwargs)
-        self.size_hint = (1, 1)
-        self.bind(size=self.update)
+# class MapWidget(Screen):
+#     def __init__(self, **kwargs):
+#         super(MapWidget, self).__init__(**kwargs)
+#         self.size_hint = (1, 1)
+#         self.bind(size=self.update)
         
-        # Example map data
-        self.add_widget(Label(text="Map Data: Latitude = 40.7128, Longitude = -74.0060", color=(0, 0, 0, 1)))
+#         # Example map data
+#         self.add_widget(Label(text="Map Data: Latitude = 40.7128, Longitude = -74.0060", color=(0, 0, 0, 1)))
 
-        with self.canvas:
-            Color(1, 1, 1, 1)  # White background
-            self.rect = Rectangle(pos=self.pos, size=self.size)
+#         with self.canvas:
+#             Color(1, 1, 1, 1)  # White background
+#             self.rect = Rectangle(pos=self.pos, size=self.size)
     
-    def update(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
+#     def update(self, *args):
+#         self.rect.pos = self.pos
+#         self.rect.size = self.size
         
 class ScreenManagement(ScreenManager):
     pass
--- weather.py
+++ weather.py
@@ -1,22 +1,21 @@
 import requests
 import time
 
-def temp_converter(kelvin_temp):
-    return (kelvin_temp - 273.15)*(9/5)+32
+class Weather:
+    def temp_converter(self, kelvin_temp):
+        return (kelvin_temp - 273.15)*(9/5)+32
+    
+    def get_weather(self, city: str):
+        api_key = '77ccac5efac3aa383ec4263be801fe61'
+        weather_info_url = f'https://api.openweathermap.org/data/2.5/weather?q={city},co,us&appid={api_key}'
 
-city = 'denver'
-api_key = '77ccac5efac3aa383ec4263be801fe61'
-# geo_url = f'https://api.openweathermap.org/geo/1.0/direct?q={city},co,us&limit=1&appid={api_key}'
-# geo_info = (requests.get(geo_url)).json()
-# lat,lon = geo_info[0]['lat'],geo_info[0]['lon']
+        weather_info = (requests.get(weather_info_url)).json()
+        weather_condition = (weather_info['weather'][0]['main'])
+        weather_temp = (round(self.temp_converter(weather_info['main']['temp'])))
 
-# weather_info_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
-weather_info_url = f'https://api.openweathermap.org/data/2.5/weather?q={city},co,us&appid={api_key}'
-
-weather_info = (requests.get(weather_info_url)).json()
-weather_condition = (weather_info['weather'][0]['main'])
-weather_temp = (round(temp_converter(weather_info['main']['temp'])))
-
-print(f'Location: {city}\n'
-      f'Weather: {weather_condition}\n'
-      f'Temperature: {weather_temp}\n')
+        print(f'Location: {city}\n'
+            f'Weather: {weather_condition}\n'
+            f'Temperature: {weather_temp}\n')
+        
+        return (weather_condition, weather_temp)
+        
