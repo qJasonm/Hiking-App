384ec77b39fcbcbd0e6de66a2c3056f38b469d64
qJasonm <117887620+qJasonm@users.noreply.github.com>
2024-04-10 16:31:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,34 @@ from kivy.uix.label import Label
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from weather import Weather
 from kivy.clock import mainthread
 from kivy.config import Config
 import socket
 import threading
 
-HOST = '127.0.0.1'
+import requests
+import time
+
+class Weather:
+    def temp_converter(self, kelvin_temp):
+        return (kelvin_temp - 273.15)*(9/5)+32
+    
+    def get_weather(self, city: str):
+        api_key = '77ccac5efac3aa383ec4263be801fe61'
+        weather_info_url = f'https://api.openweathermap.org/data/2.5/weather?q={city},co,us&appid={api_key}'
+
+        weather_info = (requests.get(weather_info_url)).json()
+        weather_condition = (weather_info['weather'][0]['main'])
+        weather_temp = (round(self.temp_converter(weather_info['main']['temp'])))
+
+        print(f'Location: {city}\n'
+            f'Weather: {weather_condition}\n'
+            f'Temperature: {weather_temp}\n')
+        
+        return (weather_condition, weather_temp)
+        
+
+HOST = '192.168.0.111'
 PORT = 8080
 Builder.load_file('my.kv')
 
